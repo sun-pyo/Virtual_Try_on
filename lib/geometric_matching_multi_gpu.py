9abae54c3b7a7d81479bf227bb8addb3989bbc30
sun-pyo <sunpyu97@gmail.com>
2021-03-11 20:26:18 +0900

    view -> reshape

--- lib/geometric_matching_multi_gpu.py
+++ lib/geometric_matching_multi_gpu.py
@@ -123,7 +123,7 @@ class FeatureRegression(nn.Module):
 
     def forward(self, x):
         x = self.conv(x)
-        x = x.view(x.size(0), -1)
+        x = x.reshape(x.size(0), -1)
         x = self.linear(x)
         x = self.tanh(x)
         return x
